849fea4a076d08c2dcd207abdf3c67b07d23d5ac
Qinyi Zhou <qz23@imperial.ac.uk>
2023-10-09 07:37:14 +0800

    fini PB: matrix transpose

--- cachelab/trans.c
+++ cachelab/trans.c
@@ -19,9 +19,20 @@ int is_transpose(int M, int N, int A[N][M], int B[M][N]);
  *     searches for that string to identify the transpose function to
  *     be graded. 
  */
+
+// 8*8 Blocking
 char transpose_submit_desc[] = "Transpose submission";
 void transpose_submit(int M, int N, int A[N][M], int B[M][N])
 {
+    for (int i = 0; i < M; i += 8) {
+        for (int j = 0; j < N; j += 8) {
+            for (int k = i; k < i + 8; k ++) {
+                for (int m = j; m < j + 8; m++) {
+                    B[m][k] = A[k][m];
+                }
+            }
+        }
+    }
 }
 
 /* 
@@ -46,6 +57,98 @@ void trans(int M, int N, int A[N][M], int B[M][N])
 
 }
 
+char trans_desc_3232[] = "Matrix 32*32";
+void trans_3232(int M, int N, int A[N][M], int B[M][N])
+{
+   int v0, v1, v2, v3, v4, v5, v6, v7;
+    for (int i = 0; i < N; i += 8) {
+        for (int j = 0; j < M; j += 8) {
+            for (int k = i; k < i + 8; k ++) {
+                v0 = A[k][j]; v1 = A[k][j + 1];
+                v2 = A[k][j + 2]; v3 = A[k][j + 3];
+                v4 = A[k][j + 4]; v5 = A[k][j + 5];
+                v6 = A[k][j + 6]; v7 = A[k][j + 7];
+                B[j][k] = v0; B[j + 1][k] = v1;
+                B[j + 2][k] = v2; B[j + 3][k] = v3;
+                B[j + 4][k] = v4; B[j + 5][k] = v5;
+                B[j + 6][k] = v6; B[j + 7][k] = v7;
+            }
+        }
+    }
+}
+
+char trans_desc_6464[] = "Matrix 64*64";
+void trans_6464(int M, int N, int A[N][M], int B[M][N])
+{
+   int v0, v1, v2, v3, v4, v5, v6, v7;
+    for (int i = 0; i < N; i += 8) {
+        for (int j = 0; j < M; j += 8) {
+            for (int k = i; k < i + 4; k++) {
+                v0 = A[k][j]; v1 = A[k][j + 1]; v2 = A[k][j + 2]; v3 = A[k][j + 3];
+                v4 = A[k][j + 4]; v5 = A[k][j + 5]; v6 = A[k][j + 6]; v7 = A[k][j + 7];
+                B[j][k] = v0; B[j + 1][k] = v1; B[j + 2][k] = v2; B[j + 3][k] = v3;
+                B[j][k + 4] = v4; B[j + 1][k + 4] = v5; B[j + 2][k + 4] = v6; B[j + 3][k + 4] = v7;
+            }
+
+            for (int m = j; m < j + 4; m++) {
+                v0 = A[i + 4][m]; v1 = A[i + 5][m]; v2 = A[i + 6][m]; v3 = A[i + 7][m]; 
+                v4 = B[m][i + 4]; v5 = B[m][i + 5]; v6 = B[m][i + 6]; v7 = B[m][i + 7];
+
+                B[m][i + 4] = v0; B[m][i + 5] = v1; B[m][i + 6] = v2; B[m][i + 7] = v3;
+                B[m + 4][i] = v4; B[m + 4][i + 1] = v5; B[m + 4][i + 2] = v6; B[m + 4][i + 3] = v7;
+            }
+
+            for (int x = i + 4; x < i + 8; x++) {
+                v0 = A[x][j + 4]; v1 = A[x][j + 5]; v2 = A[x][j + 6]; v3 = A[x][j + 7];
+                B[j + 4][x] = v0; B[j + 5][x] = v1; B[j + 6][x] = v2; B[j + 7][x] = v3;
+            }
+        }
+    }
+}
+
+char trans_desc_6167[] = "Matrix 61*67";
+void trans_6167(int M, int N, int A[N][M], int B[M][N])
+{
+   int i, j, k, n, v0, v1, v2, v3, v4, v5, v6, v7;
+    for (i = 0; i < N - 16; i += 16) {
+        for (j = 0; j < M - 16; j += 16) {
+            for (k = i; k < i + 16; k++) {
+                v0 = A[k][j]; v1 = A[k][j + 1];
+                v2 = A[k][j + 2]; v3 = A[k][j + 3];
+                v4 = A[k][j + 4]; v5 = A[k][j + 5];
+                v6 = A[k][j + 6]; v7 = A[k][j + 7];
+
+                B[j][k] = v0; B[j + 1][k] = v1;
+                B[j + 2][k] = v2; B[j + 3][k] = v3;
+                B[j + 4][k] = v4; B[j + 5][k] = v5;
+                B[j + 6][k] = v6; B[j + 7][k] = v7;
+
+                v0 = A[k][j + 8]; v1 = A[k][j + 9];
+                v2 = A[k][j + 10]; v3 = A[k][j + 11];
+                v4 = A[k][j + 12]; v5 = A[k][j + 13];
+                v6 = A[k][j + 14]; v7 = A[k][j + 15];
+
+                B[j + 8][k] = v0; B[j + 9][k] = v1;
+                B[j + 10][k] = v2; B[j + 11][k] = v3;
+                B[j + 12][k] = v4; B[j + 13][k] = v5;
+                B[j + 14][k] = v6; B[j + 15][k] = v7;
+            }
+        }
+    }
+
+    for (k = i; k < N; k++) {
+        for (n = 0; n < M; n++) {
+            B[n][k] = A[k][n];
+        }
+    }
+
+    for (k = 0; k < i; k++) {
+        for (n = j; n < M; n++) {
+            B[n][k] = A[k][n];
+        }
+    }
+}
+
 /*
  * registerFunctions - This function registers your transpose
  *     functions with the driver.  At runtime, the driver will
@@ -61,6 +164,11 @@ void registerFunctions()
     /* Register any additional transpose functions */
     registerTransFunction(trans, trans_desc); 
 
+    registerTransFunction(trans_3232, trans_desc_3232);
+
+    registerTransFunction(trans_6464, trans_desc_6464); 
+
+    registerTransFunction(trans_6167, trans_desc_6167);
 }
 
 /* 
@@ -81,4 +189,3 @@ int is_transpose(int M, int N, int A[N][M], int B[M][N])
     }
     return 1;
 }
-
